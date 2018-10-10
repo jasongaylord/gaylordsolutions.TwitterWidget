608cdc45c71a4e2712272026322cb92d0c44e717
Jason Gaylord <Jason.Gaylord@biberk.com>
2018-10-10 12:58:47 -0400

    Add the tweet struct to the project

---
+++ cloudscribe.LatestTweets/Models/TweetStruct.cs
@@ -0,0 +1,15 @@
+﻿namespace LatestTwitter.Models
+{
+    public class TweetStruct
+    {
+        public string Username { get; set; }
+
+        public string Message { get; set; }
+
+        public string Avatar { get; set; }
+
+        public string Timestamp { get; set; }
+
+        public string Id { get; set; }
+    }
+}
