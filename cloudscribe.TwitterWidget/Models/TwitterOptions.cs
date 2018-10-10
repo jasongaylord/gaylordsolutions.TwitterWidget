366fa52340fad48ab90c240be80ac2422b588a20
Jason Gaylord <Jason.Gaylord@biberk.com>
2018-10-10 15:46:06 -0400

    Updated default settings for twitter options

--- cloudscribe.TwitterWidget/Models/TwitterOptions.cs
+++ cloudscribe.TwitterWidget/Models/TwitterOptions.cs
@@ -12,11 +12,11 @@ namespace cloudscribe.TwitterWidget.Models
         public int Count { get; set; } = 5;
         public int CacheMinutes { get; set; } = 5;
         public bool ShowAvatars { get; set; } = false;
-        public bool ShowUsername { get; set; } = false;
-        public bool ShowTimestamps { get; set; } = false;
-        public bool ShowMentionsAsLinks { get; set; } = false;
-        public bool ShowHashtagsAsLinks { get; set; } = false;
-        public bool ShowTimestampsAsLinks { get; set; } = false;
+        public bool ShowUsername { get; set; } = true;
+        public bool ShowTimestamps { get; set; } = true;
+        public bool ShowMentionsAsLinks { get; set; } = true;
+        public bool ShowHashtagsAsLinks { get; set; } = true;
+        public bool ShowTimestampsAsLinks { get; set; } = true;
         public string HashTagsFilter { get; set; } = "";
         public bool ShowReplies { get; set; } = false;
         [Required]
