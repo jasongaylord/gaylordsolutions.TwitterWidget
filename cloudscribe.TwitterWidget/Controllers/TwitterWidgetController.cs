d9aa6fbce41b5b38ac5f9d9d8be48c4a06e61793
Jason Gaylord <Jason.Gaylord@biberk.com>
2018-10-11 09:17:41 -0400

    Refactored the controller to be cleaner.

--- cloudscribe.TwitterWidget/Controllers/TwitterWidgetController.cs
+++ cloudscribe.TwitterWidget/Controllers/TwitterWidgetController.cs
@@ -1,6 +1,5 @@
 ﻿using cloudscribe.TwitterWidget.Models;
 using cloudscribe.TwitterWidget.Services;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Logging;
@@ -14,16 +13,14 @@ namespace cloudscribe.TwitterWidget.Controllers
     [ApiExplorerSettings(IgnoreApi = true)]
     public class TwitterWidgetController : Controller
     {
-        protected IHostingEnvironment HostingEnvironment { get; private set; }
         protected ITwitterService TwitterService { get; private set; }
         protected TwitterOptions TwitterOptions { get; private set; }
         protected ILogger Log { get; private set; }
         private MemoryCache _cache;
         private static readonly string CacheKey = "TwitterCache_";
 
-        public TwitterWidgetController(IHostingEnvironment appEnv, ITwitterService twitterService, ILogger<TwitterWidgetController> logger, TwitterCache cache, IOptions<TwitterOptions> options = null)
+        public TwitterWidgetController(ITwitterService twitterService, ILogger<TwitterWidgetController> logger, TwitterCache cache, IOptions<TwitterOptions> options = null)
         {
-            HostingEnvironment = appEnv;
             TwitterService = twitterService;
             Log = logger;
             _cache = cache.Cache;
@@ -46,10 +43,18 @@ namespace cloudscribe.TwitterWidget.Controllers
             var key = CacheKey + TwitterOptions.Username + "_RetrieveTweets";
             var results = new List<TweetStruct>();
 
-            if (!_cache.TryGetValue(key, out results))
+            try
             {
-                results = await TwitterService.RetrieveTweets(TwitterOptions, key);
-                _cache.Set(key, results, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(TwitterOptions.CacheMinutes)));
+                if (!_cache.TryGetValue(key, out results))
+                {
+                    results = await TwitterService.RetrieveTweets(TwitterOptions, key);
+                    _cache.Set(key, results, new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(TwitterOptions.CacheMinutes)));
+                }
+            }
+            catch (Exception ex)
+            {
+                Log.LogError(ex, "Error occurred in RetrieveTweets().");
+                return new JsonResult("{\"error\":\"The TwitterWidget threw an error. Check the logs for more details.\"}");
             }
 
             return new JsonResult(results);
